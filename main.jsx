5acb24415bfcae9f7de47ca18b3621fbae672597
SatishDGurav <satish19283@gmail.com>
2026-07-07 11:50:30 +0530

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import ExcelFreelanceSite from "./ExcelFreelanceSite.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <ExcelFreelanceSite />
+  </React.StrictMode>
+);
